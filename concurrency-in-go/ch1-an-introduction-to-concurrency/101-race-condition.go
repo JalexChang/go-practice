f45d817ade264a28da54dc2898493d42a2b9c79b
JalexChang <oscay0723@gmail.com>
2018-10-28 18:07:29 +0800

    Upload sample code of chapter 1

---
+++ concurrency-in-go/ch1-an-introduction-to-concurrency/101-race-condition.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var data int
+	go func() {
+		data++
+	}()
+	if data == 0 {
+		fmt.Printf("the value is %v.\n", data)
+	}
+}
---
+++ concurrency-in-go/ch1-an-introduction-to-concurrency/102-memory-access-synchronization.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var memoryAccess sync.Mutex
+	var value int
+	go func() {
+		memoryAccess.Lock()
+		value++
+		memoryAccess.Unlock()
+	}()
+
+	memoryAccess.Lock()
+	if value == 0 {
+		fmt.Printf("the value is %v.\n", value)
+	} else {
+		fmt.Printf("the value is %v.\n", value)
+	}
+	memoryAccess.Unlock()
+}
